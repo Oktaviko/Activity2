864a9d786be7e681ccac145f34f0dbba86ed4d77
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-20 17:28:17 +0700

    membuat method q_sort yang bersifat public

--- Program.cs
+++ Program.cs
@@ -49,6 +49,52 @@ namespace Activity2
             arr[x] = arr[y];
             arr[y] = temp;
         }
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
+
+            //partition tge list into two pasrts;
+            //one containing elements less that or equal to pivot
+            //outhe containing elements greather than pivot
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
+            while (i <= j)
+            {
+                //search for an element greater than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                //search for an element less than or equal to pivot
+                while ((arr[j] > pivot) && (j <= low))
+                {
+                    j--;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j) //if the greater element is on the left of the lement
+                {
+                    //swap the element at index i wit the element at index
+                    swap(i, j);
+                    mov_count++;
+                }
+                //sort the list on the left of pivot using quick sort
+                q_sort(low, j - 1);
+                //sort the list on the right of pivot using quick sort
+                q_sort(j + 1, high);
+            }
+
+        }
 
         static void Main(string[] args)
         {

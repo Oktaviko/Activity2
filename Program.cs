7dd25986752f9038bd3d0dea650e6ac6f161509f
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-20 16:44:52 +0700

    membuat variabel integer n

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace Activity2
         private int[] arr = new int[20];
         private int cmp_count = 0; //number of comparasion
         private int mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int n;
     }
     {
         static void Main(string[] args)

7e03c374109753de146bc57f6d4aec32d870ad85
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-20 17:10:02 +0700

    penukaran elements arr x,y dan temporary

--- Program.cs
+++ Program.cs
@@ -28,9 +28,28 @@ namespace Activity2
                 else
                     Console.WriteLine("\nArray can have maximum 20 elements \n");
             }
+            Console.WriteLine("\n====================");
+            Console.WriteLine("Enter array elements");
+            Console.WriteLine("\n====================");
+
+            //get array elements
+            for (int i=0;i < n;i++)
+            {
+                Console.Write("<" + (i+1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
-    }
-    {
+        //swaps the elements at index x with the elements at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+
         static void Main(string[] args)
         {
         }

ac851297a44b91c033b6660b0b36a601b57a2ba7
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-20 17:39:58 +0700

    membuat method display

--- Program.cs
+++ Program.cs
@@ -33,9 +33,9 @@ namespace Activity2
             Console.WriteLine("\n====================");
 
             //get array elements
-            for (int i=0;i < n;i++)
+            for (int i = 0; i < n; i++)
             {
-                Console.Write("<" + (i+1) + ">");
+                Console.Write("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
@@ -93,11 +93,28 @@ namespace Activity2
                 //sort the list on the right of pivot using quick sort
                 q_sort(j + 1, high);
             }
-
         }
+        void display()
+        {
 
+            Console.WriteLine("\n-------------------");
+            Console.WriteLine("Sorted array elements");
+            Console.WriteLine("---------------------");
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(arr[j]);
+            }
+            Console.WriteLine("\nNumber of comparasions: " + cmp_count);
+            Console.WriteLine("\nNumber of data movements: " + mov_count);
+        }
+        int getSize()
+        {
+            return (n);
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }

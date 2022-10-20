13cfd89ffe8d873309af1c1447738dbe9e680c0b
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-20 16:43:56 +0700

    membuat class program dan membuat variabel int(private)

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Activity2
 {
-    internal class Program
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0; //number of data movements
+    }
     {
         static void Main(string[] args)
         {
